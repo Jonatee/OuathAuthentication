487b731b8520e905f94210598575803d1209e786
Jonatee <oluwolejonatee@gmail.com>
2025-03-26 19:31:40 +0100

    first commit

---
+++ AuthContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OuathAuthentication.Entities;
+
+namespace OuathAuthentication
+{
+    public class AuthContext : DbContext 
+    {
+        public AuthContext(DbContextOptions<AuthContext> options) : base(options) { }
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace OuathAuthentication.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = default!;
+        public string LastName { get;set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public int TimesOfLogin { get; set; }
+        public DateTime LastLoginAt { get; set; }
+
+    }
+}
